9a63c926f02af577f125f62f9726ae2745615f34
krb3p <ryankatherineburke@gmail.com>
2017-03-16 16:11:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function window(array) {
+  array
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name) {
+  var new_kittens = [...kittens, name]
+  return new_kittens
+}
+
+function prependKitten(name) {
+  var new_kittens = [name, ...kittens]
+  return new_kittens
+}
+
+function removeLastKitten() {
+  var new_kittens = kittens.slice(0, length-1)
+  return new_kittens
+}
+
+function removeFirstKitten() {
+  var new_kittens = kittens.slice(1)
+  return new_kittens
+}
